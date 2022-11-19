08794a55ddf7675ccd914d07c29b991967bfe3b2
Manish <manish@gmail.com>
2022-11-19 15:40:16 +0545

    added files in datatable is correct

---
+++ src/Components/DataTable/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class DataTable extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>DataTable</h1>;
+  }
+}
+
+export default DataTable;
