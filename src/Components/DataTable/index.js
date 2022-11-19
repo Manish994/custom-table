a5539d166171deae8f10c9ae059d0a56e594b62b
Manish <manish@gmail.com>
2022-11-19 16:46:32 +0545

    Display Table Column and Data

--- src/Components/DataTable/index.js
+++ src/Components/DataTable/index.js
@@ -37,10 +37,51 @@ class DataTable extends React.Component {
     return headerView;
   };
 
+  renderNoData = () => {
+    return (
+      <tr>
+        <td colSpan={this.header.length}>{this.noData}</td>
+      </tr>
+    );
+  };
+
+  renderContent = () => {
+    let { headers, data } = this.state;
+    let conentView = data.map((row, rowIdx) => {
+      let id = row[this.keyField];
+
+      let tds = headers.map((header, headerIdx) => {
+        let content = row[header.accessor];
+        let cell = header.cell;
+
+        if (cell) {
+          if (typeof cell === "object") {
+            if (cell.type === "image" && content) {
+              content = (
+                <img style={cell.style} src={content} alt="loading"></img>
+              );
+            }
+          } else if (typeof cell === "function") {
+            content = cell(content);
+          }
+        }
+
+        return (
+          <td key={headerIdx} data-id={id} data-row={rowIdx}>
+            {content}
+          </td>
+        );
+      });
+      return <tr key={id}>{tds}</tr>;
+    });
+    return conentView;
+  };
+
   renderTable = () => {
     let title = this.props.title || "Data-Table";
     let headerView = this.renderTableHeader();
-    let contentView = "Content Goes here";
+    let contentView =
+      this.state.data.length > 0 ? this.renderContent() : this.renderNoData();
 
     return (
       <table className="data-inner-table">
